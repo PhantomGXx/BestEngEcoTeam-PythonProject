b359c4e1d0bf52c4500b33f526ada7864ae9530b
Phantom <alirezan2u2@gmail.com>
2024-06-05 19:35:50 +0330

    Commit 3

--- EE_PROJECT_BEET_FUNCTIONS.py
+++ EE_PROJECT_BEET_FUNCTIONS.py
@@ -5,29 +5,32 @@ import pandas as pd
 import sklearn.linear_model as lm
 DATA = []
 
-def PF(F,R : float ,N):
+def PF(N,I,F=1):
     # P = F(P/F,R,N)
-    L = (1+R)**N
+    L = (1+I)**N
     return F / L
 
-def FP(P,R : float ,N):
+def FP(N,I,P=1):
     # F = P(F/P,R,N)
-    L = (1+R)**N
+    L = (1+I)**N
     return P * L
 
-def PA(A,R : float ,N):
+def PA(N,I,A=1):
     # P = A(P/A,R,N)
-    L = (1+R)**N
-    return A * ( (L-1 )/(R * L) )
+    L = (1+I)**N
+    return A * ( (L-1)/(I * L) )
 
-def AP(P,R : float ,N):
+def AP(N,I,P=1):
     # A = P(A/P,R,N)
-    L = (1+R)**N
-    return P * ( (R * L)/(L-1) )
+    L = (1+I)**N
+    return P * ( (I * L)/(L-1) )
 
-
-
-
+def PAL(N,I,J,AL=1):
+    if I!=J:
+        L = (1-((1+J)**N)*((1+I)**-N))/(I-J)
+    else:
+        L = N / (1+I)
+    return AL * L
 
 
 
